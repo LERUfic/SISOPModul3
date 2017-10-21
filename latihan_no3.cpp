d773e85af1710c20076b14c9addfe434d3d6bd69
alfian853 <alfian853@gmail.com>
2017-10-21 22:18:18 +0700

    sudah final,tapi di cek lagi

--- latihan_no3.cpp
+++ latihan_no3.cpp
@@ -2,17 +2,56 @@
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
+#include <iostream>
+using namespace std;
 
 int main(){
     system("cvlc -I rc --rc-host localhost:5666 -d");
-    sleep(1);
-    system("echo add /home/falnerz/Music/dawn.mp3 | netcat localhost 5666");
-    sleep(1);
-    system("echo play | netcat localhost 5666");
+    string arg;
+    int detik;
+    printf("For more information \'help\'\n");
+    while(1){
+        printf("command > ");
+        cin>>arg;
+        if(arg == "help"){
+            printf("Add to playlist : add [file]\n");
+            printf("Display playlist's music : list\n");
+            printf("Play music : play\n");
+            printf("Pause music : pause\n");
+            printf("Continue music : continue\n");
+            printf("Stop music : stop\n");
+        }
+        else if(arg == "playlist"){
+            system("echo playlist | netcat localhost 5666");
+        }
+        else if(arg == "add"){
+            cin>>arg;
+            arg = "echo add "+arg+" | netcat localhost 5666";
+            //konversi string C++ ke string konvensional pakai .c_str() dari stdlib
+            system(arg.c_str());
+        }
+        else if(arg == "play"|| arg=="continue"){
+            if(arg=="continue"){
+                cin>>detik;
+                sleep(detik);
+            }
+            cout<<"playing.."<<endl;
+            system("echo play | netcat localhost 5666");
+        }
+        else if(arg == "pause"){
+            cin>>detik;
+            sleep(detik);
+            system("echo pause | netcat localhost 5666");
+        }
+        else if(arg == "stop"){
+            cin>>detik;
+            sleep(detik);
+            system("echo stop | netcat localhost 5666");
+        }
 
+    }
 
-
-
+    return 0;
 
 
 }
