26b106e00ad717d9b893fdf395daf06763bea1b0
LERUfic <aguelsw@gmail.com>
2017-10-22 19:13:27 +0700

    no 2 bisa

--- 2latihan.c
+++ 2latihan.c
@@ -6,10 +6,17 @@
 
 pthread_t tid[50];
 int prime_arr[1000];
+int atas;
+int bawah;
+int n;
+int t;
+int status = 1;
+int iter =1;
 
-void *tulis(int *arg)
-{
-    for(int i = arg[0]; i <= arg[1]; i++){
+void *tulis(void *arg){
+	//printf("atas %d\n", atas);
+    printf("------>Thread %d\n", iter);
+    for(int i = bawah; i <= atas; i++){
         int isPrime = 0;
         for(int j = 2; j <= i/2; j++){
             if(i % j == 0){
@@ -17,59 +24,46 @@ void *tulis(int *arg)
                 break;
             }
         }
-        if(isPrime==0 && arg[1]!= 1){
-            printf("cekcek %d\n",arg[1]);
-            prime_arr[i]= 1;
+        if(isPrime==0 && bawah!= 1){
+            printf("%d\n",i);
+            //prime_arr[i]= 1;
         }
     }
+    status++;
 }
 
 
-/*void* baca(int *arg)
+void* baca(void *arg)
 {
-    printf("masuk baca");
-    while(status != 1)
+    while(status-1!=iter)
     {
 
     }
-    tulis(&arg);
-    status = 0;
+    bawah = atas+1;
+    atas = bawah+n/t;
+    if(atas>n) atas = n;
+}
 
-}*/
-
-int main(void)
-{
-    int n;
-    int t;
+int main(void){
     scanf("%d %d",&n, &t);
-    int temp[2];
-    temp[0]=2;
-    temp[1]=n/t;
-    for(int i=0; i<t; i++){
-
-        pthread_create(&tid[i], NULL, &tulis, &temp);
-
-        if(i==(t-1)){
-            temp[0] = temp[1]+1;
-            temp[1] = n;
-            
-        }
-        else{
-            temp[0] = temp[1]+1;
-            temp[1] = temp[1]+n/t;
-        }
+  
+    bawah=2;
+    atas=n/t;
+    for(iter=1; iter<=t; iter++){
+        pthread_create(&tid[iter], NULL, &tulis, NULL);
+        baca(NULL);
     }
 
-    for(int i=0; i<t; i++){
+    for(int i=1; i<=t; i++){
         pthread_join(tid[i], NULL);
     }
 
-    for(int i=2; i<n; i++){
+    /*for(int i=2; i<n; i++){
         //printf("check %d -> %d\n", i, prime_arr[i]);
         if(prime_arr[i]==1){
             printf("%d\n",i);
         }
-    }
+    }*/
 
     return 0;
 }
