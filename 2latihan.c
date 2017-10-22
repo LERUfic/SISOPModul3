9d6d5d5de24b062acf2fa6382ec52341494ce97e
alfian853 <alfian853@gmail.com>
2017-10-22 19:55:46 +0700

    sudah fix

--- 2latihan.c
+++ 2latihan.c
@@ -3,67 +3,58 @@
 #include<pthread.h>
 #include<stdlib.h>
 #include<unistd.h>
+#include <math.h>
 
-pthread_t tid[50];
 int prime_arr[1000];
-int atas;
-int bawah;
-int n;
-int t;
-int status = 1;
-int iter =1;
+pthread_t tid[50];
 
-void *tulis(void *arg){
-	//printf("atas %d\n", atas);
-    printf("------>Thread %d\n", iter);
-    for(int i = bawah; i <= atas; i++){
+struct ranging_t{
+int low,high;
+
+};
+void* tulis(void* args){
+    struct ranging_t *extract =(struct ranging_t*)args;
+    printf("%d %d\n",extract->low,extract->high);
+
+    for(int i =extract->low; i <=extract->high; i++){
         int isPrime = 0;
-        for(int j = 2; j <= i/2; j++){
+        for(int j = 2; j <=i/2; j++){
             if(i % j == 0){
                 isPrime = 1;
                 break;
             }
         }
-        if(isPrime==0 && bawah!= 1){
-            printf("%d\n",i);
-            //prime_arr[i]= 1;
+        if(isPrime==0){
+            prime_arr[i]= 1;
         }
     }
-    status++;
 }
 
-
-void* baca(void *arg)
-{
-    while(status-1!=iter)
-    {
-
-    }
-    bawah = atas+1;
-    atas = bawah+n/t;
-    if(atas>n) atas = n;
-}
 
 int main(void){
+    int n;
+    int t;
     scanf("%d %d",&n, &t);
-  
-    bawah=2;
-    atas=n/t;
-    for(iter=1; iter<=t; iter++){
-        pthread_create(&tid[iter], NULL, &tulis, NULL);
-        baca(NULL);
+    struct ranging_t rentang;
+    rentang.low=2;
+    rentang.high=n/t;
+    for(int i=0;i<t-1; i++){
+        //printf("> %d %d\n",rentang.low,rentang.high);
+        pthread_create(&tid[i], NULL, &tulis,(void *)&rentang);
+        pthread_join(tid[i],NULL);
+        rentang.low=1+rentang.high;
+        rentang.high+=(n/t);
     }
+    //ini untuk sisanya.terakhir ,misal 10 3 itu jadi {3 3 4},4 diambil terakhir.
+    rentang.high=n;
+    pthread_create(&tid[t-1], NULL, &tulis,(void *)&rentang);
+    pthread_join(tid[t-1],NULL);
 
-    for(int i=1; i<=t; i++){
-        pthread_join(tid[i], NULL);
-    }
-
-    /*for(int i=2; i<n; i++){
-        //printf("check %d -> %d\n", i, prime_arr[i]);
+    for(int i=2; i<n; i++){
         if(prime_arr[i]==1){
             printf("%d\n",i);
         }
-    }*/
+    }
 
     return 0;
 }
